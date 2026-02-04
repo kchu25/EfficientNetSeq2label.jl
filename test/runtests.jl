cb157f95b217745527965cfc3bb63df6cb65f763
shane <skchu@wustl.edu>
2026-02-04 18:18:19 -0500

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -161,7 +161,6 @@ using Random
         @test model_tanh.final_nonlinearity === tanh
         
         # Test create_model uses ranges.final_nonlinearity
-        Random.seed!(42)  # Use fixed seed to ensure valid architecture
         model_auto_tanh = EfficientNetSeq2label.create_model(
             (4, 41), 10, 32;
             ranges=ranges_tanh,
